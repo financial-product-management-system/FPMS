16fbde151f82be05bfc4d969a812765fe05641c9
liaoyongbiao <1105235030@qq.com>
2019-10-10 16:36:53 +0800

    1.删除没用的。

--- src/main/java/com/fpms/FpmsApplication.java
+++ src/main/java/com/fpms/FpmsApplication.java
@@ -3,11 +3,6 @@ package com.fpms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 /**
  * @author : YongBiao Liao
